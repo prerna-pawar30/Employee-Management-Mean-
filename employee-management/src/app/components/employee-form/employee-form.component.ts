e135e4797b89634fd9efee9b147faddd6ccdf158
Vishal-chaudhari12 <126507882+7775080323@users.noreply.github.com>
2025-03-03 13:36:43 +0530

    reactive

--- employee-management/src/app/components/employee-form/employee-form.component.ts
+++ employee-management/src/app/components/employee-form/employee-form.component.ts
@@ -1,14 +1,18 @@
 import { Component, Inject, OnInit } from '@angular/core';
-import { FormBuilder, FormGroup } from '@angular/forms';
+import { FormBuilder, FormGroup, ReactiveFormsModule } from '@angular/forms';
 import { ActivatedRoute, Router } from '@angular/router';
 import { EmployeeService } from '../../services/employee.service';
-
+import { MatFormFieldModule } from '@angular/material/form-field';
 @Component({
   selector: 'app-employee-form',
   templateUrl: './employee-form.component.html',
-  styleUrls: ['./employee-form.component.css']
+  styleUrls: ['./employee-form.component.css'],
+  imports: [MatFormFieldModule,ReactiveFormsModule]
 })
 export class EmployeeFormComponent implements OnInit {
+onFileSelect($event: Event,arg1: string) {
+throw new Error('Method not implemented.');
+}
   employeeForm!: FormGroup;
   employeeId: string | null = null;
 
