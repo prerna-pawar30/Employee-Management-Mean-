3366ac2907d976a216cba55118dc6d153c50f3ab
prerna-pawar30 <preranapawar808@gmail.com>
2025-03-05 12:42:02 +0530

    admin dashboard

---
+++ employee-management/src/model/employee.ts
@@ -0,0 +1,14 @@
+export class employeeModel {
+    
+  name: string;
+  email: string;
+  address: string;
+  dob: string;
+
+  constructor() {
+    this.name = '';
+    this.email = '';
+    this.address = '';
+    this.dob = '';
+  }
+}
