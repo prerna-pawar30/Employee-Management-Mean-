15409451b439d3dbdd0111e114bac0fa3ebeb1e7
Vishal-chaudhari12 <126507882+7775080323@users.noreply.github.com>
2025-03-05 23:47:49 +0530

    list

--- employee-management/src/app/footer/footer.component.ts
+++ employee-management/src/app/footer/footer.component.ts
@@ -1,8 +1,12 @@
 import { Component } from '@angular/core';
-
+import { MatIcon, MatIconModule } from '@angular/material/icon';
+import { MatToolbarModule } from '@angular/material/toolbar';
 @Component({
   selector: 'app-footer',
   templateUrl: './footer.component.html',
-  styleUrls: ['./footer.component.css']
+  styleUrls: ['./footer.component.css'],
+  imports:[MatToolbarModule,MatIconModule]
 })
-export class FooterComponent {}
+export class FooterComponent {
+currentYear: any;
+}
