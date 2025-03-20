7d6b01cfa9dfe99f14f5d7b01c48ce6281dc5bd4
Vishal-chaudhari12 <126507882+7775080323@users.noreply.github.com>
2025-03-20 11:50:02 +0530

    bootstrap

--- employee-management/src/app/components/header/header.component.ts
+++ employee-management/src/app/components/header/header.component.ts
@@ -2,18 +2,22 @@ import { Component, inject, Inject } from '@angular/core';
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { MatIconModule } from '@angular/material/icon';
 import { Router, RouterLink } from '@angular/router';
+import { MatMenuModule } from '@angular/material/menu';
+import { Location } from '@angular/common';
 import { AuthService } from '../../services/auth.service';
 import { CommonModule } from '@angular/common';
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
-  styleUrls: ['./header.component.css'],
-  imports:[MatToolbarModule,MatIconModule,RouterLink,CommonModule]
+  styleUrl:'./header.component.css',
+  imports: [MatToolbarModule, MatIconModule, RouterLink, CommonModule, MatMenuModule]
+
 })
 export class HeaderComponent {
+  router = inject(Router);
+  location = inject(Location);
   authService = inject(AuthService);
-  router =inject(Router);
-sidenav: any;
+  sidenav: any;
   async logout(): Promise<void> {
     try {
       await this.authService.logout();
@@ -22,5 +26,9 @@ sidenav: any;
       console.error('Logout failed:', error);
     }
   
+  }
+
+  goBack(): void {
+    this.location.back();
   }
 }
